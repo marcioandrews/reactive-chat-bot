f221dc371e314279a2b92f21611f5058aac7ba07
marcioandrews <marcioasordonez@gmail.com>
2022-10-18 22:02:07 -0300

    feat: any props

--- src/componets/ChatBot/ChatBotBubble.tsx
+++ src/componets/ChatBot/ChatBotBubble.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
 import { View, StyleProp, Text as RNText, TextProps as RNTextProps, TextStyle } from "react-native"
-// import { colors, typography } from "../theme"
+import { colors } from 'src/theme';
 
-export function ChatBotBubble() {
+type Sizes = keyof typeof $sizeStyles
+type Presets = keyof typeof $presets
+
+const $sizeStyles = {
+  xxl: { fontSize: 36, lineHeight: 44 } as TextStyle,
+  xl: { fontSize: 24, lineHeight: 34 } as TextStyle,
+  lg: { fontSize: 20, lineHeight: 32 } as TextStyle,
+  md: { fontSize: 18, lineHeight: 26 } as TextStyle,
+  sm: { fontSize: 16, lineHeight: 24 } as TextStyle,
+  xs: { fontSize: 14, lineHeight: 21 } as TextStyle,
+  xxs: { fontSize: 12, lineHeight: 18 } as TextStyle,
+}
+
+export interface BubbleProps extends RNTextProps {
+
+  // tx?: TxKeyPath
+
+  text?: string
+  
+  style?: StyleProp<TextStyle>
+
+  preset?: Presets
+
+
+  size?: Sizes
+
+  children?: React.ReactNode
+}
+
+export function ChatBotBubble(props: BubbleProps) {
     return (
         <View >
       
@@ -17,87 +46,47 @@ export function ChatBotBubble() {
 // });
 
 
-type Sizes = keyof typeof $sizeStyles
-// type Weights = keyof typeof typography.primary
-type Presets = keyof typeof $presets
 
-export interface TextProps extends RNTextProps {
 
-  // tx?: TxKeyPath
+// export function Text(props: TextProps) {
+//   // const { weight, size, tx, txOptions, text, children, style: $styleOverride, ...rest } = props
+//   const { size, text, children, style: $styleOverride, ...rest } = props
 
-  text?: string
-  
-  style?: StyleProp<TextStyle>
+//   const content = text || children
 
-  preset?: Presets
+//   const preset: Presets = $presets[props.preset] ? props.preset : "default"
+//   const $styles = [
+//     $rtlStyle,
+//     $presets[preset],
+//     // $fontWeightStyles[weight],
+//     $sizeStyles[size],
+//     $styleOverride,
+//   ]
 
-  // weight?: Weights
+//   return (
+//     <RNText {...rest} style={$styles}>
+//       {content}
+//     </RNText>
+//   )
+// }
 
-  size?: Sizes
 
-  children?: React.ReactNode
-}
-
-/**
- * For your text displaying needs.
- * This component is a HOC over the built-in React Native one.
- *
- * - [Documentation and Examples](https://github.com/infinitered/ignite/blob/master/docs/Components-Text.md)
- */
-export function Text(props: TextProps) {
-  // const { weight, size, tx, txOptions, text, children, style: $styleOverride, ...rest } = props
-  const { size, text, children, style: $styleOverride, ...rest } = props
-
-  const content = text || children
-
-  const preset: Presets = $presets[props.preset] ? props.preset : "default"
-  const $styles = [
-    $rtlStyle,
-    $presets[preset],
-    // $fontWeightStyles[weight],
-    $sizeStyles[size],
-    $styleOverride,
-  ]
-
-  return (
-    <RNText {...rest} style={$styles}>
-      {content}
-    </RNText>
-  )
-}
-
-const $sizeStyles = {
-  xxl: { fontSize: 36, lineHeight: 44 } as TextStyle,
-  xl: { fontSize: 24, lineHeight: 34 } as TextStyle,
-  lg: { fontSize: 20, lineHeight: 32 } as TextStyle,
-  md: { fontSize: 18, lineHeight: 26 } as TextStyle,
-  sm: { fontSize: 16, lineHeight: 24 } as TextStyle,
-  xs: { fontSize: 14, lineHeight: 21 } as TextStyle,
-  xxs: { fontSize: 12, lineHeight: 18 } as TextStyle,
-}
-
-const $fontWeightStyles = Object.entries(typography.primary).reduce((acc, [weight, fontFamily]) => {
-  return { ...acc, [weight]: { fontFamily } }
-}, {}) as Record<Weights, TextStyle>
 
 const $baseStyle: StyleProp<TextStyle> = [
   $sizeStyles.sm,
-  $fontWeightStyles.normal,
   { color: colors.text },
 ]
 
 const $presets = {
   default: $baseStyle,
 
-  bold: [$baseStyle, $fontWeightStyles.bold] as StyleProp<TextStyle>,
+  bold: [$baseStyle, ] as StyleProp<TextStyle>,
 
-  heading: [$baseStyle, $sizeStyles.xxl, $fontWeightStyles.bold] as StyleProp<TextStyle>,
+  heading: [$baseStyle, $sizeStyles.xxl, ] as StyleProp<TextStyle>,
 
-  subheading: [$baseStyle, $sizeStyles.lg, $fontWeightStyles.medium] as StyleProp<TextStyle>,
+  subheading: [$baseStyle, $sizeStyles.lg, ] as StyleProp<TextStyle>,
 
-  formLabel: [$baseStyle, $fontWeightStyles.medium] as StyleProp<TextStyle>,
+  formLabel: [$baseStyle, ] as StyleProp<TextStyle>,
 
-  formHelper: [$baseStyle, $sizeStyles.sm, $fontWeightStyles.normal] as StyleProp<TextStyle>,
+  formHelper: [$baseStyle, $sizeStyles.sm, ] as StyleProp<TextStyle>,
 }
-
-const $rtlStyle: TextStyle = isRTL ? { writingDirection: "rtl" } : {}
