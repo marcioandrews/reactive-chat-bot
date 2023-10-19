3c9c49730fdd4cb8a49859b203507b85e54c3fdf
Marcio <marcio.v.ordonez@gmail.com>
2023-10-18 22:26:16 -0300

    fix: imports and code style

--- src/pages/ChatBot/ChatBot.tsx
+++ src/pages/ChatBot/ChatBot.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { ChatBotBody } from '../../componets';
+import { ChatBotBody } from 'src/componets/ChatBot/ChatBotBody';
 
 export function ChatBot() {
-    return (
-      <View style={styles.container}>
-        <ChatBotBody/>
-      </View>
-    );
+  return (
+    <View style={styles.container}>
+      <ChatBotBody />
+    </View>
+  );
 }
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'blue',
     flex: 1,
-    width: "100%",
-  }
+    width: '100%',
+  },
 });
